ce72cb03ebaa851ddd66e94d257526be9b060d79
331323 <73833779+331323@users.noreply.github.com>
2021-10-04 14:03:12 +0200

    Landing page, niet helemaal klaar

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<?php
+$active = (isset($_GET["content"])) ? $_GET["content"] : "";
+?>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+  <div class="col-12  text-center">
+    <img src="./image/food/12.jpg" alt="test" class="img-fluid">
+  </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</nav>
